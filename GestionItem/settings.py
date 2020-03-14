25be7dc5acb66de3313e26d993d6e71739f21694
Gerardo <gerardocabrer@gmail.com>
2020-03-14 10:49:25 -0300

    Se agregaron las funciones en las vista

--- GestionItem/settings.py
+++ GestionItem/settings.py
@@ -72,7 +72,6 @@ WSGI_APPLICATION = 'GestionItem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
