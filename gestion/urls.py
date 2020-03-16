23b860c813dc9e435a0ac9a875febff9ca6de54b
waltergauto <waltergautofcb@fpuna.edu.py>
2020-03-16 12:10:56 -0300

    Se añadió nombre a url listarUsuarios

--- gestion/urls.py
+++ gestion/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('logout/', views.logout, name = 'logout'),
     path('perfil/',views.perfil, name = 'perfil'),
-    path('listarUsuarios/',views.getUsers)
+    path('listarUsuarios/',views.getUsers, name = 'getUser')
 # Create your views here.
 ]
