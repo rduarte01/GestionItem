6b40d03d3ecf67c560f1286a985d1e3aefd48275
waltergauto <waltergautofcb@fpuna.edu.py>
2020-03-16 12:09:24 -0300

    Se modificó nombre a url logout

--- gestion/urls.py
+++ gestion/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import  views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('logout/', views.logout),
+    path('logout/', views.logout, name = 'logout'),
     path('perfil/',views.perfil),
     path('listarUsuarios/',views.getUsers)
 # Create your views here.
