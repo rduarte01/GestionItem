61103f9357f358d4eddd2e0118f2c4255bef5cdb
Gerardo <gerardocabrer@gmail.com>
2020-03-20 02:50:40 -0300

    Se  agrego la pagina que lista los usuarios en espera
    Los usuarios que tenga el rol de usuario de sistema podran ver la lista de usarios que estan en estado de espera dentro del sistema.
    AQUI SI SE CARGARON TODO LOS CAMBIOS

--- gestion/models.py
+++ gestion/models.py
@@ -6,14 +6,31 @@ from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 @receiver(pre_save, sender=User)
 def set_new_user_inactive(sender, instance, **kwargs):
-    if instance._state.adding is True:
+    """Lo que hace es setear a todos los usuarios recien registrados, por defecto
+    en estado inactivo, a excepcion de los que son creados como superusuarios"""
+    if instance._state.adding is True and User.is_superuser is False:
         print("Creating Inactive User")
         instance.is_active = False
-
     else:
         print("Updating User Record")
 
 
+
+"""class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()"""
